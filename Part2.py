0b61fe4b2e65b1d773313cd197afc6e91c2de147
Richard Anderson <richard.anderson2@csuglobal.edu>
2024-09-10 19:56:49 -0500

    Version 1

---
+++ Part2.py
@@ -0,0 +1,4 @@
+num1 = float(input("Input first number "))
+num2 = float(input("Input second number "))
+print("The first number * the second number =", num1 * num2)
+print("The first number / the second number =", num1 / num2)
