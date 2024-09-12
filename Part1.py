807425031b059182759c0e1771dd774634ac3257
Richard Anderson <richard.anderson2@csuglobal.edu>
2024-09-11 21:18:34 -0500

    Version 1.1 with comments

--- Part1.py
+++ Part1.py
@@ -1,4 +1,8 @@
-num1 = float(input("Input first number "))
-num2 = float(input("Input second number "))
-print("The first number + the second number =", num1 + num2)
-print("The first number - the second number =", num1 - num2)
+#Write a program where a user enters two numbers,
+#then add the two numbers and display the result,
+#then subtract the two numbers and display the result.
+
+num1 = float(input("Input first number "))                      #User inputs the first number, convert to float.
+num2 = float(input("Input second number "))                     #User inputs the second number, convert to float.
+print("The first number + the second number =", num1 + num2)    #Add both numbers and display the result.
+print("The first number - the second number =", num1 - num2)    #Subtract both numbers and display the result.
--- Part2.py
+++ Part2.py
@@ -1,4 +1,8 @@
-num1 = float(input("Input first number "))
-num2 = float(input("Input second number "))
-print("The first number * the second number =", num1 * num2)
-print("The first number / the second number =", num1 / num2)
+#Write a program where a user enters two numbers,
+#then multiply the two numbers and display the result,
+#then divide the two numbers and display the result.
+
+num1 = float(input("Input first number "))                      #User inputs the first number, convert to float.
+num2 = float(input("Input second number "))                     #User inputs the second number, convert to float.
+print("The first number * the second number =", num1 * num2)    #Multiply both numbers and display the result.
+print("The first number / the second number =", num1 / num2)    #Divide both numbers and display the result.
